fbed70d7726e2db8fdc9af9feb647cd149aefaf5
julianoks <juao15@hampshire.edu>
2019-04-20 12:52:30 -0400

    bug fixes
    - correctly collate images
    - pass kwargs

--- autoencoder/build.py
+++ autoencoder/build.py
@@ -10,17 +10,16 @@ def get_celeba(new_img_size=[64,64]):
         image = tf.expand_dims(image, axis=0)
         image = tf.image.resize_nearest_neighbor(image, new_img_size)
         image = tf.math.divide(tf.cast(image[0], 'float32'), 255.)
-        return image
+        return image, image
     # get dataset
     folder_pattern = os.path.join(os.path.abspath(os.path.dirname(__file__)),
         'celeba-dataset', 'img_align_celeba', '*.jpg')
     dataset = tf.data.Dataset.list_files(folder_pattern)
-    dataset = dataset.shuffle(2000).map(load_image)
-    dataset = tf.data.Dataset.zip((dataset, dataset))
-    return dataset
+    dataset = dataset.map(load_image)
+    return dataset.shuffle(2000)
 
 def run(**kwargs):
-    BVAE().fit(get_celeba()).save()
+    BVAE(**kwargs).fit(get_celeba()).save()
 
 if __name__ == "__main__":
     run(epochs=1)
